c2988eb3360849c5cf4e8f6a6621ae16401c6904
Nalin <79359909+NotNalin@users.noreply.github.com>
2022-05-09 22:12:58 +0400

    smol change

--- v1.py
+++ v1.py
@@ -12,6 +12,8 @@ from discord.ui import View
 
 devs = (499112914578309120, 700195735689494558)
 
+tzone = timezone(timedelta(hours=4))
+
 giveawaytags = []
 mix = {}
 editmsg = {}
@@ -20,14 +22,13 @@ idict = {}
 ez = ["Wait... This isn't what I typed!", 'Anyone else really like Rick Astley?', 'Hey helper, how play game?', 'Sometimes I sing soppy, love songs in the car.', 'I like long walks on the beach.', 'Please go easy on me, this is my first time on discord!', "You're a great person! Do you want to chat?", 'In my free time I like to watch cat videos on Youtube', 'When I saw the witch with the potion, I knew there was trouble brewing.', 'If the Minecraft world is infinite, how is the sun spinning around it?', 'Hello everyone! I am an innocent person who loves chatting.', 'Plz give me doggo memes!', 'I heard you like Minecraft, so I built a computer in Minecraft in your Minecraft so you can Minecraft while you Minecraft', "Why can't the Ender Dragon read a book? Because he always starts at the End.", 'Maybe we can have a rematch?', 'I sometimes try to say bad things then this happens :(', 'Behold, the great and powerful, my magnificent and almighty nemisis!', 'Doin a bamboozle fren.', 'Your comebacks are godly.', 'What happens if I add chocolate milk to macaroni and cheese?', 'Can you paint with all the colors of the wind', 'Blue is greener than purple for sure', 'I had something to say, then I forgot it.', 'When nothing is right, go left.', 'I need help, teach me how to play!', 'Your personality shines brighter than the sun.', 'You are very good at the game friend.', 'I like pineapple on my pizza', 'I like pasta, do you prefer nachos?', 'I like fighting but you are truly better than me!', 'I have really enjoyed playing with you! <3', 'ILY <3', "Pineapple doesn't go on pizza!", 'Lets be friends instead of fighting okay?']
 
 
-client = commands.Bot(command_prefix='>', help_command=None,intents=discord.Intents.all())
+client = commands.Bot(command_prefix='>', help_command=None, intents=discord.Intents.all())
 
 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
     await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name='with fishes'))
-
 
 
 class GiveawayView(View):
@@ -55,7 +56,7 @@ async def on_message(message):
         return
     if bool('ez' in message.content.lower().split()) or bool('ez' == message.content.lower()) or bool('ezz' == message.content.lower()) or bool('ezzz' in message.content.lower()) or bool('e z' == message.content.lower()):
         hooks = await message.channel.webhooks()
-        hook = discord.utils.get(hooks, name='ezz')  
+        hook = discord.utils.get(hooks, name='ezz')
         if hook is None:
             hook = await message.channel.create_webhook(name='ezz', avatar=None, reason=None)
 
@@ -70,7 +71,7 @@ async def on_message(message):
         await messageid.delete()
         return
     guildid = str(message.guild.id)
-    current_time = datetime.now(timezone(timedelta(hours=4)))
+    current_time = datetime.now(tzone)
     unix_timestamp = current_time.timestamp()
     if guildid in last:
         last[guildid][str(message.author.id)] = unix_timestamp
@@ -96,7 +97,7 @@ async def on_message(message):
                         embed.set_footer(
                             text=f'Message ID: {message.id} | Author ID: {message.author.id}')
                         member = message.guild.get_member(int(str(user)))
-                        timee = datetime.now(timezone(timedelta(hours=4))).timestamp()
+                        timee = datetime.now(tzone).timestamp()
                         lastt = last[guild][user] if user in last[guild] else 0
                         if lastt == 0 or timee - lastt > 300:
                             await member.send(f"In **{message.guild.name}** {message.channel.mention}, you were mentioned with highlight word \"{msg}\"", embed=embed)
@@ -169,8 +170,8 @@ async def giveaway(ctx, time, prize):
 async def on_message_delete(message):
     if message.author.bot:
         return
-    
-    #remove if you want to snipe nqn messages
+
+    # remove if you want to snipe nqn messages
     if message.author.discriminator == '0000':
         return
 
@@ -178,8 +179,8 @@ async def on_message_delete(message):
     author = str(message.author)
     message_author_avatar = str(message.author.avatar)
     channel = str(message.channel.id)
-    timee = datetime.now(timezone(timedelta(hours=4)))
-    mix[channel] = {'content': content, 'author': author,'authorav': message_author_avatar, 'time': timee}
+    timee = datetime.now(tzone)
+    mix[channel] = {'content': content, 'author': author, 'authorav': message_author_avatar, 'time': timee}
 
     if message.attachments:
         attachment = message.attachments[0]
@@ -189,7 +190,8 @@ async def on_message_delete(message):
             mix[channel]['imgurl'] = imgurl
         else:
             mix[channel]['attachment'] = attachment.url
-        
+
+
 @client.command(aliases=['s'])
 @commands.has_any_role(773245326747500604, 734307591630356530, 734304865794392094, 888692319447023636, 888461103250669608, 861175306127933470, 874272681124589629, 960209393339731989)
 async def snipe(ctx):
@@ -212,6 +214,7 @@ async def snipe(ctx):
         imgurl = mix[channel]['imgurl']
         embed.set_image(url=imgurl)
     await ctx.reply(embed=embed, mention_author=False)
+
 
 @client.command(aliases=['dms'])
 @commands.has_any_role(773245326747500604, 734307591630356530, 734304865794392094, 888692319447023636, 888461103250669608, 861175306127933470, 874272681124589629, 860431948236587059, 960209393339731989)
@@ -246,18 +249,17 @@ async def on_message_edit(oldmsg, newmsg):
     channel = oldmsg.channel.id
     authav = oldmsg.author.avatar
     msgurl = oldmsg.jump_url
-    timee = datetime.now(timezone(timedelta(hours=4)))
+    timee = datetime.now(tzone)
 
     if oldmsg.author.bot:
         return
-    
-    #remove if you want to snipe nqn messages
+
+    # remove if you want to snipe nqn messages
     if oldmsg.author.discriminator == '0000':
         return
 
-    
     else:
-        editmsg[channel] = {'author': author, 'oldcontent': oldcontent,'newcontent': newcontent, 'authorav': authav, 'msgurl': msgurl, 'time': timee}
+        editmsg[channel] = {'author': author, 'oldcontent': oldcontent, 'newcontent': newcontent, 'authorav': authav, 'msgurl': msgurl, 'time': timee}
 
 
 @client.command(aliases=['es'])
@@ -281,6 +283,7 @@ async def esnipe(ctx):
         embed.timestamp = timee
         embed.set_author(name=f'{author}', icon_url=f'{authav}')
         await ctx.reply(embed=embed, mention_author=False)
+
 
 @client.command(aliases=['dmes'])
 @commands.has_any_role(773245326747500604, 734307591630356530, 734304865794392094, 888692319447023636, 888461103250669608, 861175306127933470, 874272681124589629, 860431948236587059, 960209393339731989)
@@ -306,6 +309,7 @@ async def dmesnipe(ctx):
         await ctx.author.send(embed=embed)
         await ctx.add_reaction('👍')
 
+
 @client.event
 async def on_reaction_add(reaction, user):
     message = await reaction.message.channel.fetch_message(reaction.message.id)
@@ -320,6 +324,7 @@ async def on_reaction_add(reaction, user):
                 await message.delete()
                 if s_user != None:
                     await snipemsg.delete()
+
 
 @client.command(aliases=['d'])
 async def delete(ctx):
@@ -337,6 +342,7 @@ async def delete(ctx):
                 await snipemsg.delete()
     else:
         await ctx.react('<a:nochamp:972351244700090408>')
+
 
 @client.event
 async def on_command_error(ctx, error):
