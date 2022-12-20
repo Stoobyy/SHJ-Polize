8c74744b77d1df90658f7b907b6b84eadbc2a133
Nalin <79359909+NotNalin@users.noreply.github.com>
2022-12-20 12:42:25 +0400

    rename file and fixed bugs

--- cogs/Funfish.py
+++ cogs/Funfish.py
@@ -4,21 +4,21 @@ import datetime
 from datetime import timedelta
 
 
-class Moderation(commands.Cog):
+class Funfish(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    def dxb_status():
+    def dxb_status(self):
         return self.client.get_guild(723259592800206940).get_member(763642116953604098).status == discord.Status.online
     
     @commands.Cog.listener()
-    async def on_member_join(member):
-        if dxb_status():
+    async def on_member_join(self, member):
+        if self.dxb_status():
             return
         channel = await self.client.fetch_channel(734011317798830111)
         await channel.send(f'Hello there,{member.mention}\nGet yourself some roles from <#767320632663998494>\nHave a great time here in the server!')
 
 
 def setup(client):
-    client.add_cog(Moderation(client))
-    print("Moderation cog loaded")
+    client.add_cog(Funfish(client))
+    print("Funfish cog loaded")
