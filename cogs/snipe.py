83f3809cf8be2469b6de98e3c66417f3c35c0737
Stooby with two y's <97161453+Stoobyy@users.noreply.github.com>
2022-12-22 20:59:35 +0400

    trolley revert

--- cogs/snipe.py
+++ cogs/snipe.py
@@ -106,8 +106,8 @@ class Snipe(commands.Cog):
 
     @commands.command(aliases=["dms"])
     @commands.check_any(commands.has_permissions(manage_messages=True), commands.has_any_role(*roles), commands.is_owner())
-    async def dmsnipe(self, ctx): #, channel: discord.TextChannel = None
-        """ if channel is None:
+    async def dmsnipe(self, ctx, channel: discord.TextChannel = None):
+        if channel is None:
             channel = ctx.channel
         channel_id = str(channel.id)
         if channel_id in deletemsg:
@@ -140,8 +140,6 @@ class Snipe(commands.Cog):
             await ctx.author.send(embed=embed, file=img)
             return
         await ctx.author.send(embed=embed)
-        await ctx.message.add_reaction("👍") """
-        await ctx.author.send(':clown:')
         await ctx.message.add_reaction("👍")
 
     @commands.slash_command(name="snipe")
