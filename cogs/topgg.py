a19a13dbdd1d14db661c292957de2a1647cc1e97
Nalin <79359909+NotNalin@users.noreply.github.com>
2022-12-24 02:18:05 +0400

    test again

--- cogs/topgg.py
+++ cogs/topgg.py
@@ -1,43 +1,35 @@
 import discord
 from discord.ext import commands
 import topgg
+import asyncio
 
+@topgg.endpoint("/dblwebhook", topgg.WebhookType.BOT)
+async def on_bot_vote(vote_data: topgg.BotVoteData, client : commands.Bot):
+    if vote_data.type == "test":
+        print(f"Received a test vote by:\n{vote_data.user}")
+    else:
+        print(f"Received a vote by:\n{vote_data.user}")
+        user = await client.fetch_user(vote_data.user)
+        await user.send("Thanks for voting for SHJ-Polize on top.gg!")
 
-
+@topgg.endpoint("/dblwebhook", topgg.WebhookType.SERVER)
+async def on_server_vote(vote_data: topgg.ServerVoteData, client : commands.Bot):
+    if vote_data.type == "test":
+        print(f"Received a test vote by:\n{vote_data.user}")
+    else:
+        print(f"Received a vote by:\n{vote_data.user}")
+        user = await client.fetch_user(vote_data.user)
+        server : discord.Guild = await client.fetch_guild(vote_data.guild)
+        await user.send(f"Thanks for voting for {server.name} on top.gg!")
 class Topgg(commands.Cog):
     def __init__(self, client : commands.Bot):
         self.client = client
+        self.webhook_manager = topgg.WebhookManager().set_data(self.client).endpoint(on_bot_vote).endpoint(on_server_vote)
+        
 
-    # @topgg.endpoint("/dblwebhook", topgg.WebhookType.BOT)
-    # async def on_vote(self, vote_data: topgg.BotVoteData):
-    #     if vote_data.type == "test":
-    #         print(f"Received a test vote by:\n{vote_data.user}")
-    #     else:
-    #         print(f"Received a vote by:\n{vote_data.user}")
-    #         user = await self.client.fetch_user(vote_data.user)
-    #         await user.send("Thanks for voting for SHJ-Polize on top.gg!")
-            
+        if not self.webhook_manager.is_running:
+            self.webhook_manager.start(port = 5000)
 
-
-    webhook_manager = topgg.WebhookManager()
-    botvote = (
-        topgg.WebhookEndpoint()
-        .type(topgg.WebhookType.BOT)
-        .route("/dblwebhook")
-        .auth("youshallnotpass")
-    )
-    # Used as decorator factory, the decorated function will still be the function itself.
-    @botvote.callback()
-    async def on_vote(self, vote_data: topgg.BotVoteData):
-        if vote_data.type == "test":
-            print(f"Received a test vote by:\n{vote_data.user}")
-        else:
-            print(f"Received a vote by:\n{vote_data.user}")
-            user = await self.client.fetch_user(vote_data.user)
-            await user.send("Thanks for voting for SHJ-Polize on top.gg!")
-
-    webhook_manager.endpoint(botvote)
-    webhook_manager.start(5000)
 
 def setup(client):
     client.add_cog(Topgg(client))
