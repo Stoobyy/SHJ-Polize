468358e205fb263eb6b9a89741858859343d4608
Stooby with two y's <97161453+Stoobyy@users.noreply.github.com>
2023-04-11 14:46:57 +0400

    Create minecraft.py

---
+++ cogs/minecraft.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from datetime import datetime, timedelta, timezone
+import asyncio
+import requests
+from mojang import MojangAPI
+
+class Mc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def skin(ctx,username='.'):
+        id=ctx.author.id
+        embed=discord.Embed()
+        embed.add_field(name='No Username Provided!',value='Please provide a username or register your account by running `>register {username} {SkyblockProfile}`\nIf you don\'t play Skyblock, you can set the SkyblockProfile to None.')
+        await ctx.send(embed=embed)
+        uuid=MojangAPI.get_uuid(username)
+        name=MojangAPI.get_username(uuid)
+        if name==None:
+            await ctx.send(f'<:Mike:882149622561243137> {username} is not a valid username.')
+            return
+        embed=discord.Embed(title=f'{name}\'s skin',description=f'[Click here for skin](https://crafatar.com/skins/{uuid})',colour=15105570)
+        embed.set_image(url=f'https://visage.surgeplay.com/full/{uuid}?width=177&height=288')
+        embed.set_thumbnail(url=f'https://visage.surgeplay.com//face/{uuid}')
+        embed.set_footer(text=f'Requested by {ctx.author}', icon_url=f'{ctx.author.avatar}')
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Mc(bot))
+    print("mc cog loaded")
