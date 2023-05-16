cdea0dcccdb12e9d9d1c994c1b30689ccc7564ea
Nalin <79359909+NotNalin@users.noreply.github.com>
2023-05-16 12:08:48 +0400

    changed ext floder inside cogs

--- cogs/topgg.py
+++ cogs/topgg.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import topgg
-from ..ext.topgg import dblclient, manager
+from .ext.topgg import manager, dblclient
 
 
 def on_autopost_success():
