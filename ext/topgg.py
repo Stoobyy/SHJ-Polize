b836bf64b62575fac7a346bc9fca1e88f570bce2
Nalin <79359909+NotNalin@users.noreply.github.com>
2024-12-01 03:24:44 +0530

    remove server endpoint

--- ext/topgg.py
+++ ext/topgg.py
@@ -28,28 +28,8 @@ async def on_bot_vote(vote_data: topgg.BotVoteData, client: bridge.Bot = topgg.d
         except:
             pass
 
-
-@topgg.endpoint("/dslwebhook", topgg.WebhookType.GUILD, auth=os.environ["SERVER_AUTH"])
-async def on_guild_vote(vote_data: topgg.GuildVoteData, client: bridge.Bot = topgg.data(bridge.Bot)):
-    user = await client.fetch_user(vote_data.user)
-    guild: discord.Guild = await client.fetch_guild(vote_data.guild)
-    if vote_data.type == "test":
-        print(f"Received a test vote by: {user} ({vote_data.user}) for {guild.name}")
-        try:
-            await user.send(f"Test vote for {guild.name} was successful!")
-        except:
-            pass
-    else:
-        print(f"Received a vote by: {user} ({vote_data.user}) for {guild.name}")
-        try:
-            await user.send(f"Thanks for voting for {guild.name} on top.gg!")
-        except:
-            pass
-
-
-
 manager = topgg.WebhookManager()
-manager.endpoint(on_bot_vote).endpoint(on_guild_vote)
+manager.endpoint(on_bot_vote)
 
 dblclient =  topgg.DBLClient(token=os.environ["TOPGG_TOKEN"], default_bot_id=969663219570462790)
 
